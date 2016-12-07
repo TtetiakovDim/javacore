fbd9de2412d449df1199b4298e63c67b7058ba3b
Tretiakov Dim <dmitrytretaykov@gmail.com>
2016-12-07 11:17:03 +0200

    19 menu

--- src/main/java/com/tretiakovdim/app/homework/lesson19/PhoneBook.java
+++ src/main/java/com/tretiakovdim/app/homework/lesson19/PhoneBook.java
@@ -9,8 +9,8 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by DmitryTretiakov on 05.12.2016.
@@ -43,12 +43,46 @@ public class PhoneBook {
     private ArrayList<PhoneBookEntry> entry;
 
     public void addCategoryToPhoneBook(String nameCategory) {
+//       Проверка на наличие
         this.category.put(nameCategory, new ArrayList<PhoneBookEntry>());
     }
 
     public void addEntryToPhoneBook(String nameCategory, String name, String phoneNumber) {
+        //       Проверка на наличие
         this.category.get(nameCategory).add(new PhoneBookEntry(name, phoneNumber));
     }
+
+    public void renameCategoryPhoneBook(String name, String phoneNumber) {
+//Проверка на наличие контакта
+    }
+
+    public void renameEntryToPhoneBook(String nameCategory, String name, String phoneNumber) {
+        //       Проверка на наличие
+    }
+
+    public void deleteCategoryPhoneBook(String nameCategory) {
+//       Проверка на наличие
+        this.category.remove(nameCategory);
+    }
+
+    public void deleteEntryPhoneBook(String nameCategory) {
+//       Проверка на наличие
+        this.category.remove(nameCategory);
+    }
+
+    public boolean checkCategoryPhoneBook(String nameCategory) {
+        if (this.category.containsKey(nameCategory)) return true;
+        else return false;
+    }
+
+    public boolean checkEntryPhoneBook(String nameCategory, String entry) {
+        if (this.category.get(nameCategory).isEmpty()) {
+            System.out.println(this.category.get(nameCategory).isEmpty());
+            return false;
+
+        } else return true;
+    }
+
 
     public void printCategoryList() {
         for (String name : category.keySet()) {
@@ -60,25 +94,18 @@ public class PhoneBook {
     }
 
     public void printEntryInCategoryList(String nameCategory) {
+//Проверка на наличие
 
-        System.out.println(" " + this.category.get(nameCategory).get(0).getName());
-        for (int i = 0; i < this.category.get(nameCategory).size(); i++) {
-            System.out.println("Name : " + this.category.get(nameCategory).get(i).getName() + " Phone number :" + this.category.get(nameCategory).get(i).getPhoneNumber());
+        if (checkCategoryPhoneBook(nameCategory)) {
+            if (this.category.get(nameCategory).isEmpty() == false) {
+                System.out.println(" " + this.category.get(nameCategory).get(0).getName());
+                for (int i = 0; i < this.category.get(nameCategory).size(); i++) {
+                    System.out.println("" + category.get(nameCategory).toString() + ": Name : " + this.category.get(nameCategory).get(i).getName() + " : Phone number :" + this.category.get(nameCategory).get(i).getPhoneNumber());
+                }
+            }
         }
     }
 
-
-    public void deleteCategoryPhoneBookEntry(String nameCategory) {
-        this.category.remove(nameCategory);
-    }
-
-    public void deleteEntryToCategoryPhoneBook(String nameCategory, String name) {
-        this.category.remove(nameCategory);
-    }
-
-    public void addPhoneBookEntry(String namey, String phoneNumber) {
-
-    }
 
     public void Search(String searchKey) {
 
@@ -92,21 +119,18 @@ public class PhoneBook {
                     System.out.println(" : Category : " + nameKey + " : Name : " + this.category.get(nameKey.toString()).get(i).getName() + " : PhoneNumber : " + this.category.get(nameKey.toString()).get(i).getPhoneNumber() + " :");
                 }
             }
-            System.out.println("search done");
+//            System.out.println("search done");
         }
     }
 
-    public boolean filesToPhoneBookReader(String fileName) {
+    public boolean readFilesToPhoneBook(String fileName) {
         BufferedReader reader;
-        
+
         try {
             reader = new BufferedReader(new FileReader(fileName));
             String fileLine;
-
-
             boolean firstLine = true;
 
-            int j=0;
             while ((fileLine = reader.readLine()) != null) {
                 if (firstLine) {
                     firstLine = false;
@@ -114,21 +138,62 @@ public class PhoneBook {
                 }
                 String[] parsLine = fileLine.trim().split("[,|;:.!\\s]+");
                 this.category.put(parsLine[0], new ArrayList<PhoneBookEntry>());
-                this.category.get(parsLine[0]).add(new PhoneBookEntry(parsLine[1]+" "+parsLine[2], parsLine[3]+" "+parsLine[4]));
-                System.out.println(parsLine[0]+" "+parsLine[1]+" "+parsLine[3]);
             }
             reader.close();
 
+            reader = new BufferedReader(new FileReader(fileName));
+            firstLine = true;
 
+            while ((fileLine = reader.readLine()) != null) {
+                if (firstLine) {
+                    firstLine = false;
+                    continue;
+                }
+                String[] parsLine = fileLine.trim().split("[,|;:.!\\s]+");
+                this.category.get(parsLine[0]).add(new PhoneBookEntry(parsLine[1] + " " + parsLine[2], parsLine[3] + " " + parsLine[4]));
+//                System.out.println(parsLine[0] + " " + parsLine[1] + " " + parsLine[3]);
+            }
+            reader.close();
 
             return false;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return false;
+
         } catch (IOException e) {
+            Logger.getLogger(PhoneBook.class.getName())
+                    .log(Level.SEVERE, null, e);
             e.printStackTrace();
             return false;
         }
 
     }
+
+    public void phoneBookWriteTofiles(String fileName) {
+        BufferedWriter writer;
+        String fileLine;
+        boolean firstLine = true;
+
+//        bw.write(System.lineSeparator());
+
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/tretiakovdim/runners/homework/lesson19/PhoneBookRanner19.java
+++ src/main/java/com/tretiakovdim/runners/homework/lesson19/PhoneBookRanner19.java
@@ -8,53 +8,174 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Arrays;
 
+import static com.tretiakovdim.runners.homework.lesson17.GuessingRunner17.checkMenu;
+import static com.tretiakovdim.runners.homework.lesson17.GuessingRunner17.userInput;
+
 /**
  * Created by DmitryTretiakov on 05.12.2016.
  */
+
 public class PhoneBookRanner19 {
 
 
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
         Map<String, ArrayList<PhoneBookEntry>> category = new HashMap<String, ArrayList<PhoneBookEntry>>();
-        phoneBook.filesToPhoneBookReader("src/test/resources/PhoneBook.csv");
-
-        phoneBook.addCategoryToPhoneBook("Frend");
-        phoneBook.addEntryToPhoneBook("Frend", "Lev", "7777777");
+        String menuItem, incomingPhone, incomingString,incomingCategore;
+        phoneBook.readFilesToPhoneBook("src/test/resources/PhoneBook.csv");
         phoneBook.addCategoryToPhoneBook("NotFrend");
-        phoneBook.addEntryToPhoneBook("NotFrend", "NotLev", "7777777");
-
-        phoneBook.addEntryToPhoneBook("NotFrend", "kolz", "7777777788");
-        phoneBook.addEntryToPhoneBook("NotFrend", "Noev", "777777766");
-        phoneBook.addEntryToPhoneBook("NotFrend", "NtL", "777777787");
         phoneBook.addEntryToPhoneBook("NotFrend", "tLev", "7777777565");
-        phoneBook.Search("kol");
-        //phoneBook.deleteCategoryPhoneBookEntry("NotFrend");
-        phoneBook.printEntryInCategoryList("NotFrend");
-        phoneBook.printCategoryList();
 
-//        list1.add(new PhoneBookEntry("Ivan1", "7777"));
-//        category.put("1",new ArrayList<PhoneBookEntry>());
-//         list1.add(new PhoneBookEntry("Ivan2", "55555"));
-//        category.put("2", list1);
-//        list2.add(new PhoneBookEntry("Ivan3", "88888"));
-//        category.put("3", list2);
-//        list2.add(new PhoneBookEntry("Ivan4", "777776"));
-//        category.put("4", list2);
-//        category.get("1").add(new PhoneBookEntry("Ivan5", "5777776"));
-//        category.get("4").add(new PhoneBookEntry("Ivan5", "5777776"));
-//        for (Map.Entry entry : category.entrySet()) {
-//            System.out.println(entry.getKey() + ", " + entry.getValue().toString());
+        //phoneBook.deleteCategoryPhoneBookEntry("NotFrend");
+
+//        if (phoneBook.checkCategoryPhoneBook(incomingString)){
+//else {
+//                System.out.println("This category is not available, add it? ");
+//                System.out.println("1. Yes  ");
+//                System.out.println("2. Not ");
+//                menuItem = userInput();
 //
-//        }
+//                if (checkMenu(menuItem)) {
+//                    if (menuItem.contains("1")) {
+//                        phoneBook.addCategoryToPhoneBook(incomingString);
+//                        System.out.println(" Contact created ");
+//                    }
+//                    if (menuItem.contains("2")) {}
 //
-//        for (String name : category.keySet()) {
-//
-//            String key = name.toString();
-//            String value = category.get(name).toString();
-//
-//            System.out.println(key + " " + value);
-//        }
+//                    else{ System.out.println("This category is not available, add it? ");
+
+
+        System.out.println("My Phone book. ");
+        boolean again = true;
+        do {
+            phoneBook.printCategoryList();
+
+            System.out.println("1. Enter the category ");
+            System.out.println("2. Add the new category  ");
+            System.out.println("3. Rename the category ");
+            System.out.println("4. Delete the category ");
+            System.out.println("5. Search by phone number or name(Search by all)");
+            System.out.println("6. Exit.");
+
+            menuItem = userInput();
+//Подменю начало
+            if (checkMenu(menuItem)) {
+//1 ---------------------------------------------------------------------------------------------------------
+                if (menuItem.contains("1")) {
+                    System.out.println("Enter a category name ");
+                    incomingCategore = userInput();
+
+
+                    do {
+                        if (phoneBook.checkCategoryPhoneBook(incomingCategore)) {
+
+                            phoneBook.printEntryInCategoryList(incomingCategore);
+                            again = false;
+                            System.out.println("      Contact list ");
+                            System.out.println("1. Add contact  ");
+                            System.out.println("2. Rename contact  ");
+                            System.out.println("3. Delete contact  ");
+                            System.out.println("9. Back to the list category");
+                            System.out.println(" ");
+
+                            menuItem = userInput();
+
+                            if (checkMenu(menuItem)) {
+                                if (menuItem.contains("1")) {
+                                    System.out.println(" Enter a new contact  ");
+                                    incomingString = userInput();
+                                    if (phoneBook.checkEntryPhoneBook(incomingCategore,incomingString)==false){
+                                        System.out.println(" Enter phone  ");
+                                        incomingPhone= userInput();
+                                        phoneBook.addEntryToPhoneBook(incomingCategore,incomingString,incomingPhone);
+                                        System.out.println(" Contact created ");
+                                    }
+
+
+                                }
+                                if (menuItem.contains("2")) {
+                                    System.out.println("Enter a contact to rename ");
+
+                                    incomingCategore = userInput();
+                                    System.out.println(" Enter a new name for contact ");
+                                    System.out.println(" contact rename ");
+                                    System.out.println(" ");
+                                }
+                                if (menuItem.contains("3")) {
+                                    System.out.println("Enter a contact name to delete");
+                                    incomingCategore = userInput();
+                                    System.out.println("Contact delete");
+                                    System.out.println(" ");
+
+                                }
+                                if (menuItem.contains("9")) {
+                                    System.out.println(" ");
+                                    again = true;
+                                }
+                            } else System.out.println("Not correct, ente 1 or 9.  ");
+
+
+                        } else {
+                            System.out.println("This category is not available, add it? ");
+                            System.out.println("1. Yes  ");
+                            System.out.println("7. Not ");
+                            menuItem = userInput();
+                            if (checkMenu(menuItem)) {
+                                if (menuItem.contains("1")) {
+                                    phoneBook.addCategoryToPhoneBook(incomingCategore);
+                                    System.out.println(" Category add  ");
+                                    again = false;
+                                }
+                            }
+                        }
+
+                    } while (again != true);
+                }
+//1----------------------------------------------------------------------------------------------------------------
+
+                if (menuItem.contains("2")) {
+                    System.out.println("Enter a new category name ");
+                    incomingCategore = userInput();
+                    if (phoneBook.checkCategoryPhoneBook(incomingCategore) == false) {
+                        phoneBook.addCategoryToPhoneBook(incomingCategore);
+                        System.out.println(" Category created ");
+                        System.out.println(" ");
+                    }
+                    System.out.println("This category is available and not Add ");
+                }
+                if (menuItem.contains("3")) {
+                    System.out.println("Enter a new category name");
+                    incomingCategore = userInput();
+                    System.out.println(" Category rename ");
+                    System.out.println(" ");
+                }
+                if (menuItem.contains("4")) {
+                    System.out.println("Enter a category name to delete");
+                    incomingCategore = userInput();
+                    if (phoneBook.checkCategoryPhoneBook(incomingCategore) == true) {
+                        phoneBook.deleteCategoryPhoneBook(incomingCategore);
+                        System.out.println("Category delete");
+                        System.out.println(" ");
+                    } else {
+                        System.out.println("This category is not available and not delete.");
+                    }
+
+                }
+                if (menuItem.contains("5")) {
+                    System.out.println("Enter an expression for search");
+                    incomingString = userInput();
+                    phoneBook.Search(incomingString);
+                    System.out.println(" ");
+                }
+                if (menuItem.contains("6")) {
+                    System.out.println("Bye");
+                    again = false;
+                }
+
+            } else System.out.println("Not correct, ente 1 or 9.  ");
+// Подменю конець
+        }
+        while (again);
 
 
     }
